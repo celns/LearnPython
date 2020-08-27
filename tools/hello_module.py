e9885d5170d52b751301a4a61723d6ce64e287bc
celns <celns@outlook.com>
2020-08-28 01:30:48 +0800

    模块
    Signed-off-by: celns <celns@outlook.com>

---
+++ tools/hello_module.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'hello module'
+
+__author__ = 'celns'
+
+
+def speak():
+    print('hello module')
+
+    
+if __name__ == '__main__':
+    speak()
